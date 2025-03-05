e24fb16d445f890c10580800809b41ef2d738c71
marco76tv <marco.sottana@gmail.com>
2025-03-05 17:16:09 +0100

    .

--- git_up_noai.sh
+++ git_up_noai.sh
@@ -9,60 +9,9 @@ me=$( readlink -f -- "$0";)
 branch=$1
 where=$(pwd)
 
-# Funzione per copiare e rinominare le cartelle
-move_config() {
-  local dir_name="$1"  # Il nome della cartella (es. "Config")
-  local dir_name_lower=$(echo "$dir_name" | tr '[:upper:]' '[:lower:]')  # Trasforma il nome in minuscolo
-
-  # Verifica che la cartella esista
-  if [ -d "$dir_name" ] && [ -d "$dir_name_lower" ]; then
-    # Copia tutto il contenuto di dir_name_lower nella cartella passata come parametro
-    cp -r "$dir_name_lower"/* "$dir_name"/
-    
-    # Rinomina dir_name_lower in dir_name e la cartella passata in dir_name_lower
-    mv "$dir_name_lower" "$dir_name_lower"_old
-    mv "$dir_name" "$dir_name_lower"
-    echo "Operazione completata per $dir_name."
-  else
-    echo "Errore: Le cartelle $dir_name o $dir_name_lower non esistono."
-  fi
-
-  # Aggiungi il controllo per rinominare la cartella _old in caso di conflitto
-  if [ -d "$dir_name_lower"_old ] && [ ! -d "$dir_name_lower" ]; then
-    echo "Rinominando $dir_name_lower_old in $dir_name_lower..."
-    mv "$dir_name_lower"_old "$dir_name_lower"
-  fi
-}
-
-
-
 git submodule update --progress --init --recursive --force --merge --rebase --remote
 git submodule foreach "$me" "$branch"
 find . -type f -name "*:Zone.Identifier" -exec rm -f {} \;
-move_config "App"
-move_config "Config"
-move_config "Database"
-move_config "Resources"
-move_config "Routes"
-move_config "Tests"
-
-
-git config core.fileMode false
-git config advice.submoduleMergeConflict false
-git config core.ignorecase false
-git add --renormalize -A
-#git add -A && aicommits  || echo '---------------------------empty'
-git add -A && git commit -am "up"  || echo '---------------------------empty'
-git push origin $branch -u --progress 'origin' || git push --set-upstream origin $branch
-echo "-------- END PUSH[$where ($branch)] ----------";
-git checkout $branch --
-git branch --set-upstream-to=origin/$branch $branch
-git branch -u origin/$branch
-git merge $branch
-echo "-------- END BRANCH[$where ($branch)] ----------";
-git submodule update --progress --init --recursive --force --merge --rebase --remote
-git checkout $branch --
-git pull origin $branch --autostash --recurse-submodules --allow-unrelated-histories --prune --progress -v --rebase
 #old branches
 #git push origin --delete cs0.2.03
 #git push origin --delete cs0.2.04
@@ -72,6 +21,24 @@ git pull origin $branch --autostash --recurse-submodules --allow-unrelated-histo
 #git push origin --delete cs0.2.08
 #git push origin --delete cs0.2.09
 #git push origin --delete cs0.2.10
+
+git config core.fileMode false
+git config advice.submoduleMergeConflict false
+git config core.ignorecase false
+git add --renormalize -A
+#git add -A && aicommits  || echo '---------------------------empty'
+git add -A && git commit -am "up"  || echo '---------------------------empty'
+git push origin $branch -u --progress 'origin' || git push --set-upstream origin $branch
+git rebase --continue || echo 'no rebasing'
+echo "-------- END PUSH[$where ($branch)] ----------";
+git checkout $branch --
+git branch --set-upstream-to=origin/$branch $branch
+git branch -u origin/$branch
+git merge $branch
+echo "-------- END BRANCH[$where ($branch)] ----------";
+git submodule update --progress --init --recursive --force --merge --rebase --remote
+git checkout $branch --
+git pull origin $branch --autostash --recurse-submodules --allow-unrelated-histories --prune --progress -v --rebase
 sed -i -e 's/\r$//' "$me"
 echo "-------- END PULL[$where ($branch)] ----------";
 
